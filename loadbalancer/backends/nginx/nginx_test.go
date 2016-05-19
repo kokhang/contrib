2827e0e39e7f7b1ff59c29acaee58ac5d8a7ea66
Steve Leon <kokhang@gmail.com>
2016-05-19 12:56:42 -0700

    Adding unit test for nginx backend controller

---
+++ loadbalancer/backends/nginx/nginx_test.go
@@ -0,0 +1,72 @@
+package nginx
+
+import (
+	"reflect"
+	"testing"
+	
+	"k8s.io/contrib/loadbalancer/backends"
+)
+
+func TestGetNGINXConfigFileName(t *testing.T) {
+	path := "path"
+	name := "filename"
+	expected := "path/filename.conf"
+	if result := getNGINXConfigFileName(path, name); result != expected {
+		t.Errorf("getNGINXConfigFileName(%q, %q) returned %q. Expected %q. ", path, name, result, expected)
+	}
+}
+
+func TestGenerateNGINXCfg(t *testing.T) {
+	certPath := "/etc/nginx/ssl"
+	name := "nginxApp"
+	configObject := generateExampleBackendObject()
+	
+	expectedNginxConfig := NGINXConfig {
+		Upstream: Upstream{
+			Name: "nginxApp-localhost-helloApp",
+			UpstreamServer: UpstreamServer{
+				Address: "helloApp", 
+				Port: "8080",
+			},
+		}, 
+		Server: Server{
+			Name: "localhost",
+			BindIP: "127.0.0.1",
+			BindPort: "80",
+			Location: Location{
+				Path: "/hello",
+				Upstream: Upstream{
+					Name: "nginxApp-localhost-helloApp",
+					UpstreamServer: UpstreamServer{
+						Address: "helloApp", 
+						Port: "8080",
+					},
+				},
+			},
+		},
+	}
+	
+	nginxConfig := generateNGINXCfg(certPath, name, configObject)
+	if !reflect.DeepEqual(nginxConfig, expectedNginxConfig) {
+		t.Error(
+			"In generateNGINXCfg()",
+			"expected", expectedNginxConfig,
+			"got", nginxConfig,
+		)
+	}	
+}
+
+func generateExampleBackendObject() backends.BackendConfig{
+	backendConfig := backends.BackendConfig{
+		Host: "localhost",
+		Namespace: "default",
+		BindIp: "127.0.0.1",
+		BindPort: 80,
+		TargetServiceName: "helloApp",
+		TargetServiceId: "123",
+		TargetPort: 8080,
+		SSL: false,
+		Path: "/hello",
+	}
+	return backendConfig
+}
